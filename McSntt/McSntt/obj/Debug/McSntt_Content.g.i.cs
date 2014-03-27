c92fdfdb341bd107d84f589002679c719409fca6
trampetramp <sfrandsen8@gmail.com>
2014-03-27 12:11:10 +0100

    Person Class - Created
    Titel

--- McSntt/McSntt/obj/Debug/McSntt_Content.g.i.cs
+++ McSntt/McSntt/obj/Debug/McSntt_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("finalimages/sundetlogo.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/sundetlogo.png")]
 
 
