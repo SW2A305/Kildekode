4f7b6a3b8fdcf1892a49d774bd466bf56a328b75
nsmed <nikolajsmed@hotmail.com>
2014-03-29 14:13:15 +0100

    Tabs tilføjet

---
+++ McSntt/McSntt/MemberList.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using McSntt.Models;
+
+namespace McSntt
+{
+    /// <summary>
+    /// Interaction logic for MemberList.xaml
+    /// </summary>
+    public partial class MemberList : Window
+    {
+        public MemberList()
+        {
+            // Create testmembers in order to display data. 
+            var testMember = new SailClubMember
+            {
+                FirstName = "Troels",
+                LastName = "Kroegh",
+                Address = "Scoresbysundvej 8",
+                Postcode = "9210",
+                Cityname = "Aalborg SØ",
+                Email = "HalloHallo@gmail.com",
+                PhoneNumber = "12345678",
+                Gender = Gender.Male,
+                MemberId = 1337,
+                Position = Positions.Admin,
+                DateOfBirth = new DateTime(1994, 06, 13)
+            };
+            var testMember2 = new SailClubMember
+            {
+                FirstName = "Søren",
+                LastName = "Kroegh",
+                Address = "Scoresbysundvej 8",
+                Postcode = "9000",
+                Cityname = "Aalborg SØ",
+                Email = "HalloHallo@gmail.com",
+                PhoneNumber = "12345678",
+                Gender = Gender.Male,
+                MemberId = 1337,
+                Position = Positions.Admin,
+                DateOfBirth = new DateTime(1994, 06, 13)
+            };
+
+            // Put members into a list
+            var listOfMembers = new List<SailClubMember>();
+            listOfMembers.Add(testMember);
+            listOfMembers.Add(testMember2);
+
+
+            // Set the list as the current DataContext
+            this.DataContext = listOfMembers;
+
+            InitializeComponent();
+        }
+
+        private void ExitButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
