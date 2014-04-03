0fccdd7985da86fce1f7ece393f84a8a079869bd
Tristan Bendixen <angelod1981@gmail.com>
2014-04-04 01:31:19 +0200

    - Added a few DAL interfaces and accompanying classes. - Implemented XML-parser to use the DAL instead. - Created a test class, but with no contents yet. This will come tomorrow, though.

---
+++ McSntt/McSntt/DataAbstractionLayer/IGenericDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McSntt.DataAbstractionLayer
+{
+    public interface IGenericDal<T> where T : class
+    {
+        bool Create(params T[] items);
+        bool Update(params T[] items);
+        bool Delete(params T[] items);
+        IEnumerable<T> GetAll();
+    }
+}
---
+++ McSntt/McSntt/Helpers/XmlMemberParser.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Xml;
+using McSntt.DataAbstractionLayer;
+using McSntt.Models;
+
+namespace McSntt.Helpers
+{
+    public class XmlMemberParser
+    {
+        private readonly ISailClubMemberDal _sailClubMemberDal;
+
+        /// <summary>
+        /// </summary>
+        public XmlMemberParser() : this(new SailClubMemberEfDal()) {}
+
+        /// <summary>
+        /// </summary>
+        /// <param name="dal"></param>
+        public XmlMemberParser(ISailClubMemberDal dal)
+        {
+            this._sailClubMemberDal = dal;
+        }
+
+        /// <summary>
+        ///     Imports members from an XML file that has been exported from an Access DB.
+        /// </summary>
+        /// <param name="xmlFilePath">The path of the XML file to import from.</param>
+        /// <remarks>
+        ///     It might be a good idea to execute this on a separate thread, just to make it scale better. It won't be important
+        ///     with only a few members, of course, but still.
+        /// </remarks>
+        public void ImportMembersFromXmlFile(String xmlFilePath)
+        {
+            SailClubMember member = null;
+
+            // Parse file
+            using (XmlReader reader = XmlReader.Create(xmlFilePath))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        String tagName = reader.Name;
+
+                        if (tagName == "tblMembers")
+                        {
+                            member = new SailClubMember();
+                        }
+                        else if (member != null && reader.Read())
+                        {
+                            switch (tagName)
+                            {
+                                case "Id":
+                                    member.SetMemberId(reader.Value.Trim());
+                                    break;
+
+                                case "FirstName":
+                                    member.FirstName = reader.Value.Trim();
+                                    break;
+
+                                case "LastName":
+                                    member.LastName = reader.Value.Trim();
+                                    break;
+
+                                case "Address":
+                                    member.Address = reader.Value.Trim();
+                                    break;
+
+                                case "PostCode":
+                                    member.Postcode = reader.Value.Trim();
+                                    break;
+
+                                case "CityName":
+                                    member.Cityname = reader.Value.Trim();
+                                    break;
+
+                                case "Birthdate":
+                                    member.DateOfBirth = reader.Value.Trim();
+                                    break;
+
+                                case "IsMale":
+                                    member.Gender = reader.Value.Trim().Equals("0") ? Gender.Female : Gender.Male;
+                                    break;
+                            }
+                        }
+                    }
+                    else if (reader.NodeType == XmlNodeType.EndElement && reader.Name == "tblMembers")
+                    {
+                        // TODO Perhaps we should be checking for the return value here, as it will be false if something went wrong... But what should we do if it does?
+                        this._sailClubMemberDal.Create(member);
+
+                        member = null;
+                    }
+                }
+            }
+        }
+    }
+}
