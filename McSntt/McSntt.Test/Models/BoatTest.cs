41a78d985ddddf1338a7b9e7d17c4f86fc527f2b
Troels Kroegh <sataikage@gmail.com>
2014-04-13 14:45:07 +0200

    initial

---
+++ McSntt/McSntt.Test/Models/BoatTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using McSntt.Models;
+using NUnit.Framework;
+
+namespace McSntt.Test.Models
+{
+    [TestFixture]
+    class BoatTest : McSntt.Models.
+    {
+        [Test]
+        public void ID_TwoInstances_diffirentIds()
+        {
+            var instanceOne = new Boat();
+            var instanceTwo = new Boat();
+
+            Assert.AreNotEqual(instanceOne.id, instanceTwo.id);
+        }
+    }
+}
---
+++ McSntt/McSntt/Models/Boat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McSntt.Models
+{
+    class Boat
+    {
+        public Boat()
+        {
+            // Asign the next id to the new instance of the boat
+            id = _nextId++;
+        }
+        // Boats have an unique ID number
+        private static int _nextId;
+        public int id { get; private set; }
+    }
+}
