a55e24a3f0f64d0bca7f3eb187c3f9363684b216
Tristan Bendixen <angelod1981@gmail.com>
2014-04-24 01:27:44 +0200

    Added API methods for RegularTrip DAL.

--- McSntt/McSntt/DataAbstractionLayer/IRegularTripDal.cs
+++ McSntt/McSntt/DataAbstractionLayer/IRegularTripDal.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using McSntt.Models;
 
 namespace McSntt.DataAbstractionLayer
 {
     public interface IRegularTripDal : IGenericDal<RegularTrip>
     {
-        IEnumerable<RegularTrip> GetRegularTrips();
+        IEnumerable<RegularTrip> GetRegularTrips(Predicate<RegularTrip> predicate);
+        IEnumerable<RegularTrip> GetReservationsForBoat(Boat boat, bool onlyFuture = true);
+        IEnumerable<RegularTrip> GetReservationsForBoat(Boat boat, DateTime fromDateTime, DateTime toDateTime);
+        bool CanMakeReservation(Boat boat, DateTime departureTime, DateTime expectedArrivalTime);
+        bool CanMakeReservation(RegularTrip trip);
     }
 }
