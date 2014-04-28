b98507c0cb2a808679046ef101f801a7ed190a72
TrampeTramp <sfrand12@student.aau.dk>
2014-04-28 16:02:37 +0200

    UI til CrewCreate og LogbookCreate
    Titel

--- McSntt/McSntt/Views/Windows/CreateCrewWindow.xaml.cs
+++ McSntt/McSntt/Views/Windows/CreateCrewWindow.xaml.cs
@@ -5,33 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using McSntt.Views.Windows;
 
 namespace McSntt.Views.Windows
 {
     /// <summary>
-    /// Interaction logic for CreateLogbookWindow.xaml
+    /// Interaction logic for CreatCrewWindow.xaml
     /// </summary>
-    public partial class CreateLogbookWindow : Window
+    public partial class CreateCrewWindow : Window
     {
-        public CreateLogbookWindow()
+        public CreateCrewWindow()
         {
             InitializeComponent();
-
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var createCrewWindow = new CreateCrewWindow();
-            createCrewWindow.ShowDialog();
-
         }
     }
 }
