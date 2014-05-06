a0fd7007f791cad2fd8661b1543a5c44cf3b656f
TriXXeD <marc_thorgersen@hotmail.com>
2014-05-06 21:05:34 +0200

    studyStudent skulle gerne virke nu

--- McSntt/McSntt/Views/UserControls/studyStudent.xaml.cs
+++ McSntt/McSntt/Views/UserControls/studyStudent.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using McSntt.Helpers;
+using McSntt.Models;
 
 namespace McSntt.Views.UserControls
 {
@@ -23,6 +25,29 @@ namespace McSntt.Views.UserControls
         public studyStudent()
         {
             InitializeComponent();
+            progressGrid.IsEnabled = false;
+            practicalCheck1.IsChecked = ((StudentMember) GlobalInformation.CurrentUser).Drabant;
+            practicalCheck2.IsChecked = ((StudentMember) GlobalInformation.CurrentUser).Gaffelrigger;
+            practicalCheck3.IsChecked = ((StudentMember)GlobalInformation.CurrentUser).Night;
+            theoryCheck1.IsChecked = ((StudentMember)GlobalInformation.CurrentUser).RopeWorks;
+            theoryCheck2.IsChecked = ((StudentMember)GlobalInformation.CurrentUser).Navigation;
+            theoryCheck3.IsChecked = ((StudentMember)GlobalInformation.CurrentUser).Motor;
+            practicalCheck1.IsEnabled = false;
+            practicalCheck2.IsEnabled = false;
+            practicalCheck3.IsEnabled = false;
+            theoryCheck1.IsEnabled = false;
+            theoryCheck2.IsEnabled = false;
+            theoryCheck3.IsEnabled = false;
+            teamName.Text = ((StudentMember) GlobalInformation.CurrentUser).AssociatedTeam.Name;
+            if (((StudentMember) GlobalInformation.CurrentUser).AssociatedTeam.Level == Team.ClassLevel.First)
+            {
+                level.Text = "1. års sejlerhold";
+            }
+            else
+            {
+                level.Text = "2. års sejlerhold";
+            }
+
         }
 
        
