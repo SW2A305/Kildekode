d12e10cb72b783fd7c6311646bc87f9a5e7cc888
Tristan Bendixen <angelod1981@gmail.com>
2014-05-13 01:30:33 +0200

    Added a convenience method.

--- McSntt/McSntt/DataAbstractionLayer/IRegularTripDal.cs
+++ McSntt/McSntt/DataAbstractionLayer/IRegularTripDal.cs
@@ -8,5 +8,6 @@ namespace McSntt.DataAbstractionLayer
     public interface IRegularTripDal : IGenericDal<RegularTrip>
     {
         bool CanMakeReservation(Boat boat, DateTime departureTime, DateTime arrivalTime);
+        bool CanMakeReservation(RegularTrip trip);
     }
 }
